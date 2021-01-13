74aff34e7a5afda973dc048b8fee6af082d0b7d1
David Badura <d.a.badura@gmail.com>
2021-01-13 12:11:02 +0100

    rename projection method

--- src/Projection/Projection.php
+++ src/Projection/Projection.php
@@ -9,7 +9,7 @@ use Patchlevel\EventSourcing\Aggregate\AggregateChanged;
 interface Projection
 {
     /** @return iterable<class-string<AggregateChanged>, string> */
-    public function getHandledMessages(): iterable;
+    public function handledEvents(): iterable;
 
     public function drop(): void;
 }
