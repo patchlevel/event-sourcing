71be346b6c1b9c9d31c22ebd54f54d9c77ffc534
David Badura <david.badura@patchlevel.de>
2021-11-29 12:21:16 +0100

    add database commands

---
+++ src/Console/Command/DatabaseDropCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patchlevel\EventSourcing\Console\Command;
+
+use Patchlevel\EventSourcing\Store\DoctrineStore;
+use Patchlevel\EventSourcing\Store\Store;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DatabaseDropCommand extends Command
+{
+    private Store $store;
+
+    public function __construct(Store $store)
+    {
+        parent::__construct();
+
+        $this->store = $store;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('event-sourcing:database:drop')
+            ->setDescription('create eventstore database');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $console = new SymfonyStyle($input, $output);
+        $store = $this->store;
+
+        if (!$store instanceof DoctrineStore) {
+            $console->error('store is not supported');
+
+            return 1;
+        }
+
+        $connection = $store->connection();
+        $databaseName = $connection->getParams()['dbname'];
+
+        $connection->createSchemaManager()->dropDatabase($databaseName);
+
+        return 0;
+    }
+}
