1d98ef4df8fb8b32b0052aa1793410299f954905
David Badura <david.badura@patchlevel.de>
2021-12-03 16:39:04 +0100

    address comment

--- src/Repository/WrongAggregate.php
+++ src/Repository/WrongAggregate.php
@@ -8,8 +8,14 @@ use function sprintf;
 
 final class WrongAggregate extends RepositoryException
 {
+    /**
+     * @param class-string $aggregateClass
+     * @param class-string $expected
+     */
     public function __construct(string $aggregateClass, string $expected)
     {
-        parent::__construct(sprintf('aggregate "%s::%s" not found', $aggregateClass, $expected));
+        parent::__construct(
+            sprintf('Wrong aggregate given: got "%s" but expected "%s"', $aggregateClass, $expected)
+        );
     }
 }
