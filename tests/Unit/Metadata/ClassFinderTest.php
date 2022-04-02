85ae3d1c5206d16c5b8ceb9b6f8f44d534ef5b33
David Badura <david.badura@patchlevel.de>
2022-04-02 12:57:09 +0200

    use smaller directory for the class finder test

--- tests/Unit/Metadata/ClassFinderTest.php
+++ tests/Unit/Metadata/ClassFinderTest.php
@@ -20,28 +20,8 @@ class ClassFinderTest extends TestCase
     public function testLoadDirectory(): void
     {
         $finder = new ClassFinder();
-        $classes = $finder->findClassNames([__DIR__ . '/../Fixture']);
+        $classes = $finder->findClassNames([__DIR__ . '/Projection']);
 
-        $this->assertEquals([
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\Dummy2Projection',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\DummyProjection',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\Email',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\EmailNormalizer',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\EmptyEvent',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\Message',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\MessageDeleted',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\MessageId',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\MessagePublished',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\NotNormalizedProfileCreated',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\Profile',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileCreated',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileId',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileIdNormalizer',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileInvalid',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileVisited',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileWithSnapshot',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\ProfileWithSuppressAll',
-            'Patchlevel\\EventSourcing\\Tests\\Unit\\Fixture\\SimpleEvent',
-        ], $classes);
+        $this->assertEquals(['Patchlevel\\EventSourcing\\Tests\\Unit\\Metadata\\Projection\\AttributeProjectionMetadataFactoryTest'], $classes);
     }
 }
