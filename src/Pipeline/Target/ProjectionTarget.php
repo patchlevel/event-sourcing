5bddda0eec299fe4cb1134a55f94a54dd0f2ce54
David Badura <david.badura@patchlevel.de>
2022-04-13 14:23:31 +0200

    remove dupplicated code in projection target and use default projection handler

--- src/Pipeline/Target/ProjectionTarget.php
+++ src/Pipeline/Target/ProjectionTarget.php
@@ -5,43 +5,26 @@ declare(strict_types=1);
 namespace Patchlevel\EventSourcing\Pipeline\Target;
 
 use Patchlevel\EventSourcing\EventBus\Message;
-use Patchlevel\EventSourcing\Metadata\Projection\AttributeProjectionMetadataFactory;
 use Patchlevel\EventSourcing\Metadata\Projection\ProjectionMetadataFactory;
+use Patchlevel\EventSourcing\Projection\DefaultProjectionHandler;
 use Patchlevel\EventSourcing\Projection\Projection;
-
-use function array_key_exists;
 
 final class ProjectionTarget implements Target
 {
-    private Projection $projection;
-    private ProjectionMetadataFactory $metadataFactory;
+    private DefaultProjectionHandler $projectionHandler;
 
     public function __construct(
         Projection $projection,
         ?ProjectionMetadataFactory $projectionMetadataFactory = null
     ) {
-        $this->projection = $projection;
-        $this->metadataFactory = $projectionMetadataFactory ?? new AttributeProjectionMetadataFactory();
+        $this->projectionHandler = new DefaultProjectionHandler(
+            [$projection],
+            $projectionMetadataFactory
+        );
     }
 
     public function save(Message $message): void
     {
-        $metadata = $this->metadataFactory->metadata($this->projection::class);
-        $event = $message->event();
-
-        if (!array_key_exists($event::class, $metadata->handleMethods)) {
-            return;
-        }
-
-        $handlerMetadata = $metadata->handleMethods[$event::class];
-        $method = $handlerMetadata->methodName;
-
-        if ($handlerMetadata->passMessage) {
-            $this->projection->$method($message);
-
-            return;
-        }
-
-        $this->projection->$method($event);
+        $this->projectionHandler->handle($message);
     }
 }
