8b2e607d98b4607b22679b7dcfea039ec84bdbd4
David Badura <david.badura@patchlevel.de>
2022-12-16 22:23:11 +0100

    add projectionist remove confirmation

--- src/Console/Command/ProjectionistRemoveCommand.php
+++ src/Console/Command/ProjectionistRemoveCommand.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Patchlevel\EventSourcing\Console\Command;
 
+use Patchlevel\EventSourcing\Console\OutputStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 #[AsCommand(
     'event-sourcing:projectionist:remove',
@@ -16,7 +18,20 @@ final class ProjectionistRemoveCommand extends ProjectionistCommand
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new OutputStyle($input, $output);
+
         $criteria = $this->projectionCriteria($input);
+
+        if ($criteria->ids === null) {
+            $result = $io->askQuestion(
+                new ConfirmationQuestion('do you want to remove all projections?', false)
+            );
+
+            if ($result === false) {
+                return 1;
+            }
+        }
+
         $this->projectionist->remove($criteria);
 
         return 0;
