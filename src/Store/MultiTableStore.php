bb399984697e6ab35331b0f1bc7b9a48d532ac35
David Badura <david.badura@patchlevel.de>
2023-05-03 12:52:44 +0200

    fix missing default values for not nullable fields

--- src/Store/MultiTableStore.php
+++ src/Store/MultiTableStore.php
@@ -306,9 +306,11 @@ final class MultiTableStore extends DoctrineStore implements StreamableStore, Sc
         $table->addColumn('recorded_on', Types::DATETIMETZ_IMMUTABLE)
             ->setNotnull(false);
         $table->addColumn('new_stream_start', Types::BOOLEAN)
-            ->setNotnull(true);
+            ->setNotnull(true)
+            ->setDefault(false);
         $table->addColumn('archived', Types::BOOLEAN)
-            ->setNotnull(true);
+            ->setNotnull(true)
+            ->setDefault(false);
         $table->addColumn('custom_headers', Types::JSON)
             ->setNotnull(true);
 
