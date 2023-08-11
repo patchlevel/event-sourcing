da44934dc78c9637794a4bc39e6ab8ee9b6cf122
David Badura <david.badura@patchlevel.de>
2023-08-11 14:03:21 +0200

    replace event system by throw by error flag

--- src/Projection/Projectionist/Projectionist.php
+++ src/Projection/Projectionist/Projectionist.php
@@ -9,10 +9,18 @@ use Patchlevel\EventSourcing\Projection\Projection\ProjectionCriteria;
 
 interface Projectionist
 {
-    public function boot(ProjectionCriteria $criteria = new ProjectionCriteria(), int|null $limit = null): void;
+    public function boot(
+        ProjectionCriteria $criteria = new ProjectionCriteria(),
+        int|null $limit = null,
+        bool $throwByError = false,
+    ): void;
 
     /** @param positive-int $limit */
-    public function run(ProjectionCriteria $criteria = new ProjectionCriteria(), int|null $limit = null): void;
+    public function run(
+        ProjectionCriteria $criteria = new ProjectionCriteria(),
+        int|null $limit = null,
+        bool $throwByError = false,
+    ): void;
 
     public function teardown(ProjectionCriteria $criteria = new ProjectionCriteria()): void;
 
