9b5d0a9495625fb446b0db00923202c3f09ed788
David Badura <david.badura@patchlevel.de>
2024-01-25 16:42:41 +0100

    add missing exception message

--- src/Metadata/AggregateRoot/ClassIsNotAnAggregate.php
+++ src/Metadata/AggregateRoot/ClassIsNotAnAggregate.php
@@ -6,6 +6,13 @@ namespace Patchlevel\EventSourcing\Metadata\AggregateRoot;
 
 use Patchlevel\EventSourcing\Metadata\MetadataException;
 
+use function sprintf;
+
 final class ClassIsNotAnAggregate extends MetadataException
 {
+    /** @param class-string $class */
+    public function __construct(string $class)
+    {
+        parent::__construct(sprintf('class %s is not an aggregate root', $class));
+    }
 }
