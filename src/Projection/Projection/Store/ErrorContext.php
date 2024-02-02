64cc693d1c03e535e7d1c8f439b3608ee9721cc1
David Badura <david.badura@patchlevel.de>
2024-02-02 15:50:01 +0100

    improve projection errors and traces

--- src/Projection/Projection/Store/ErrorContext.php
+++ src/Projection/Projection/Store/ErrorContext.php
@@ -6,9 +6,15 @@ namespace Patchlevel\EventSourcing\Projection\Projection\Store;
 
 use Throwable;
 
+use function array_walk_recursive;
+use function get_resource_type;
+use function is_object;
+use function is_resource;
+use function sprintf;
+
 /**
  * @psalm-type Trace = array{file?: string, line?: int, function?: string, class?: string, type?: string, args?: array}
- * @psalm-type Context = array{message: string, code: int|string, file: string, line: int, trace: list<Trace>}
+ * @psalm-type Context = array{class: class-string, message: string, code: int|string, file: string, line: int, trace: list<Trace>}
  */
 final class ErrorContext
 {
@@ -29,11 +35,35 @@ final class ErrorContext
     private static function transformThrowable(Throwable $error): array
     {
         return [
+            'class' => $error::class,
             'message' => $error->getMessage(),
             'code' => $error->getCode(),
             'file' => $error->getFile(),
             'line' => $error->getLine(),
-            'trace' => $error->getTrace(),
+            'trace' => self::transformTrace($error->getTrace()),
         ];
+    }
+
+    /**
+     * @param list<Trace> $trace
+     *
+     * @return list<Trace>
+     */
+    private static function transformTrace(array $trace): array
+    {
+        array_walk_recursive($trace, static function (mixed &$value): void {
+            if (is_object($value)) {
+                $value = sprintf('object(%s)', $value::class);
+            }
+
+            if (!is_resource($value)) {
+                return;
+            }
+
+            $value = sprintf('resource(%s)', get_resource_type($value));
+        });
+
+        /** @var list<Trace> $trace */
+        return $trace;
     }
 }
