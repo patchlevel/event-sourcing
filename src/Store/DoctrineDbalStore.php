1a6ecdb1abae9e3965da6de6709c07dd43048b12
David Badura <david.badura@patchlevel.de>
2024-02-04 11:15:50 +0100

    add string length

--- src/Store/DoctrineDbalStore.php
+++ src/Store/DoctrineDbalStore.php
@@ -238,12 +238,15 @@ final class DoctrineDbalStore implements Store, ArchivableStore, SchemaConfigura
             ->setAutoincrement(true)
             ->setNotnull(true);
         $table->addColumn('aggregate', Types::STRING)
+            ->setLength(255)
             ->setNotnull(true);
         $table->addColumn('aggregate_id', Types::STRING)
+            ->setLength(36)
             ->setNotnull(true);
         $table->addColumn('playhead', Types::INTEGER)
             ->setNotnull(true);
         $table->addColumn('event', Types::STRING)
+            ->setLength(255)
             ->setNotnull(true);
         $table->addColumn('payload', Types::JSON)
             ->setNotnull(true);
