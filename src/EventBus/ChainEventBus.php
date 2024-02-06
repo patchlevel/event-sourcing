58be58013f7a73ef3c32edb1a54848d74d5c30b9
David Badura <david.badura@patchlevel.de>
2024-02-06 13:33:31 +0100

    add chain event bus

---
+++ src/EventBus/ChainEventBus.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patchlevel\EventSourcing\EventBus;
+
+final class ChainEventBus implements EventBus
+{
+    public function __construct(
+        private readonly iterable $eventBuses,
+    ) {
+    }
+
+    public function dispatch(Message ...$messages): void
+    {
+        foreach ($this->eventBuses as $eventBus) {
+            $eventBus->dispatch(...$messages);
+        }
+    }
+}
