43c7100b28a56b6dc664339ccc66aa3dab05a638
Daniel Badura <daniel.badura@patchlevel.de>
2024-02-27 14:47:12 +0100

    Fix benchmark tests

--- tests/Benchmark/SplitStreamBench.php
+++ tests/Benchmark/SplitStreamBench.php
@@ -7,6 +7,7 @@ namespace Patchlevel\EventSourcing\Tests\Benchmark;
 use Patchlevel\EventSourcing\Aggregate\AggregateRootId;
 use Patchlevel\EventSourcing\EventBus\DefaultEventBus;
 use Patchlevel\EventSourcing\EventBus\EventBus;
+use Patchlevel\EventSourcing\EventBus\Serializer\DefaultHeadersSerializer;
 use Patchlevel\EventSourcing\Metadata\Event\AttributeEventMetadataFactory;
 use Patchlevel\EventSourcing\Repository\DefaultRepository;
 use Patchlevel\EventSourcing\Repository\MessageDecorator\SplitStreamDecorator;
@@ -40,6 +41,10 @@ final class SplitStreamBench
         $this->store = new DoctrineDbalStore(
             $connection,
             DefaultEventSerializer::createFromPaths([__DIR__ . '/BasicImplementation/Events']),
+            DefaultHeadersSerializer::createFromPaths([
+                __DIR__ . '/../../src',
+                __DIR__ . '/BasicImplementation/Events',
+            ]),
             'eventstore',
         );
 
