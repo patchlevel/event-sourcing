87e3ce322638c9e2059ce770724d6f922a4b4887
David Badura <david.badura@patchlevel.de>
2024-12-05 14:29:18 +0100

    add early out in subscription engine

--- Makefile
+++ Makefile
@@ -18,11 +18,11 @@ cs: vendor                                                                      
 
 .PHONY: phpstan
 phpstan: vendor                                                                 ## run phpstan static code analyser
-	vendor/bin/phpstan analyse
+	php -d memory_limit=312M vendor/bin/phpstan analyse
 
 .PHONY: phpstan-baseline
 phpstan-baseline: vendor                                                        ## run phpstan static code analyser
-	vendor/bin/phpstan analyse --generate-baseline
+	php -d memory_limit=312M vendor/bin/phpstan analyse --generate-baseline
 
 .PHONY: psalm
 psalm: vendor                                                                   ## run psalm static code analyser
--- tests/Unit/Subscription/Engine/SubscriptionManagerTest.php
+++ tests/Unit/Subscription/Engine/SubscriptionManagerTest.php
@@ -13,6 +13,8 @@ use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 
+use function iterator_to_array;
+
 /** @covers \Patchlevel\EventSourcing\Subscription\Engine\SubscriptionManager */
 final class SubscriptionManagerTest extends TestCase
 {
@@ -107,7 +109,7 @@ final class SubscriptionManagerTest extends TestCase
         $manager = new SubscriptionManager($store->reveal());
         $result = $manager->find($criteria);
 
-        self::assertSame([$subscription], $result);
+        self::assertSame([$subscription], iterator_to_array($result));
     }
 
     public function testFindForUpdateWithoutLock(): void
@@ -126,7 +128,7 @@ final class SubscriptionManagerTest extends TestCase
             return $subscriptions;
         });
 
-        self::assertSame([$subscription], $result);
+        self::assertSame([$subscription], iterator_to_array($result));
     }
 
     public function testFindForUpdateWithLock(): void
@@ -151,6 +153,6 @@ final class SubscriptionManagerTest extends TestCase
             return $subscriptions;
         });
 
-        self::assertSame([$subscription], $result);
+        self::assertSame([$subscription], iterator_to_array($result));
     }
 }
