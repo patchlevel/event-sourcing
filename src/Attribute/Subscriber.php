3d781dba26b733c4902530a4b997b188f8f267bc
David Badura <david.badura@patchlevel.de>
2025-02-17 17:03:32 +0100

    Replace psalm phinal plguin with phpstan extension phpat.
    This is also a first step to replace deptrac in favor of phpat as we think this tool is better suited for our usecase.

--- src/Attribute/Subscriber.php
+++ src/Attribute/Subscriber.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Patchlevel\EventSourcing\Attribute;
 
 use Attribute;
-use Cspray\Phinal\AllowInheritance;
 use Patchlevel\EventSourcing\Subscription\RunMode;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-#[AllowInheritance('You can create specific attributes with default group and run mode')]
 class Subscriber
 {
     public function __construct(
---
+++ tests/Architecture/FinalClassesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patchlevel\EventSourcing\Tests\Architecture;
+
+use Patchlevel\EventSourcing\Attribute\Subscriber;
+use PHPat\Selector\Selector;
+use PHPat\Test\Builder\Rule;
+use PHPat\Test\PHPat;
+
+final class FinalClassesTest
+{
+    public function testFinalClasses(): Rule
+    {
+        return PHPat::rule()
+            ->classes(
+                Selector::AllOf(
+                    Selector::inNamespace('Patchlevel\EventSourcing'),
+                    Selector::NOT(Selector::isAbstract()),
+                    Selector::NOT(Selector::isInterface()),
+                    Selector::NOT(Selector::classname(Subscriber::class)),
+                ),
+            )
+            ->shouldBeFinal();
+    }
+}
