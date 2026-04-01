8c75ca8e6dff5441d2f32abc5d1f7632e9a08ca2
David Badura <david.badura@patchlevel.de>
2026-04-01 15:29:29 +0200

    improve task types

--- src/Subscription/Cleanup/Dbal/DropTableTask.php
+++ src/Subscription/Cleanup/Dbal/DropTableTask.php
@@ -6,6 +6,10 @@ namespace Patchlevel\EventSourcing\Subscription\Cleanup\Dbal;
 
 final class DropTableTask
 {
+    /**
+     * @param non-empty-string      $table
+     * @param non-empty-string|null $connectionName
+     */
     public function __construct(
         public readonly string $table,
         public readonly string|null $connectionName = null,
