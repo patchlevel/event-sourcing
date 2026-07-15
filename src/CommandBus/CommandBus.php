8618c3af8a475461fb9cebf6671d1f08beaaec72
Nicolo Singer <nicolo@whatwedo.ch>
2026-07-15 12:52:36 +0200

    Add `Throwable` to `CommandBus::dispatch` @throws
    Handlers can throw any exception, and without declaring it, catching one forces `@phpstan-ignore catch.neverThrown` everywhere, which is a bad pattern.
    
    Signed-off-by: Nicolo Singer <nicolo@whatwedo.ch>

--- src/CommandBus/CommandBus.php
+++ src/CommandBus/CommandBus.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Patchlevel\EventSourcing\CommandBus;
 
+use Throwable;
+
 interface CommandBus
 {
-    /** @throws HandlerNotFound */
+    /**
+     * @throws HandlerNotFound
+     * @throws Throwable
+     */
     public function dispatch(object $command): void;
 }
